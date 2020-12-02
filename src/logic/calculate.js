fbb29d018d740ae8e4cb8e19dccd6822810303d3
Mupa1 <mupasmail@gmail.com>
2020-12-02 19:51:45 +0100

    add calculate module logic

---
+++ src/logic/calculate.js
@@ -0,0 +1,16 @@
+const calculate = (dataObject, buttonName) => {
+  let { total, next, operation } = dataObject;
+
+  if (buttonName === 'AC') {
+    total = 0;
+    next = null;
+    operation = null;
+  } else if (buttonName === '+/-') {
+    total *= -1;
+    next *= -1;
+  }
+
+  return { total, next, operation };
+};
+
+export default calculate;
