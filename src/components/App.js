912ab4de6e14aced8542d0cde70f33c3d6614627
Mupa1 <mupasmail@gmail.com>
2020-12-02 20:19:12 +0100

    import calculate into App component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import '../App.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+// eslint-disable-next-line no-unused-vars
+import calculate from '../logic/calculate';
 
 const App = () => (
   <>
