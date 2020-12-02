98cdd0327fc9d5545362c4fb8a7430b24411dfb5
Mupa1 <mupasmail@gmail.com>
2020-12-02 12:40:38 +0100

    add ButtonPanel component

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import '../App.css';
 import Display from './Display';
+import ButtonPanel from './ButtonPanel';
 
 const App = () => (
   <div className="App">
     <Display />
+    <ButtonPanel />
   </div>
 );
 
