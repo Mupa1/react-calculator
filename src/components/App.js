ed3457694a52b87d565620d6eb1994db6d228a5d
Mupa1 <mupasmail@gmail.com>
2020-12-02 12:33:43 +0100

    add display component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import '../App.css';
+import Display from './Display';
 
 const App = () => (
-  <>
-  </>
+  <div className="App">
+    <Display />
+  </div>
 );
 
 export default App;
