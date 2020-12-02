ef03aa65cd09f203fa811511d788fe4d9531f834
Mupa1 <mupasmail@gmail.com>
2020-12-02 21:02:00 +0100

    update logic on calculate module

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,5 +1,8 @@
+import operate from './operate';
+
 const calculate = (dataObject, buttonName) => {
   let { total, next, operation } = dataObject;
+  const operators = ['+', '-', '÷', 'x', '%'];
 
   if (buttonName === 'AC') {
     total = 0;
@@ -8,6 +11,16 @@ const calculate = (dataObject, buttonName) => {
   } else if (buttonName === '+/-') {
     total *= -1;
     next *= -1;
+  } else if (operators.includes(buttonName)) {
+    operate(total, next, operation).toString();
+  } else if (buttonName === '.' && next) {
+    if (!next.includes('.')) {
+      next += '.';
+    }
+  } else if ((buttonName === '=') && (next && total)) {
+    total = operate(total, next, operation).toString();
+    next = null;
+    operation = null;
   }
 
   return { total, next, operation };
