ee6172f7c8443023c3d9cb42e3e3a2627a082f63
Mupa1 <mupasmail@gmail.com>
2020-12-03 11:24:24 +0100

    refactor App into a class

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unused-state */
 import React from 'react';
 
 import '../App.css';
@@ -6,11 +7,25 @@ import ButtonPanel from './ButtonPanel';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-const App = () => (
-  <>
-    <Display />
-    <ButtonPanel />
-  </>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <Display />
+        <ButtonPanel />
+      </>
+    );
+  }
+}
 
 export default App;
