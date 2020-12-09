4331a0fbec3a8e656cb92af136984838795efb65
Mupa1 <mupasmail@gmail.com>
2020-12-09 11:31:48 +0100

    add Nav component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Display from '../display/Display';
 import ButtonPanel from '../buttonPanel/ButtonPanel';
 import calculate from '../../logic/calculate';
 import styles from './App.module.css';
+import Nav from '../nav/Nav';
 
 const App = () => {
   const [state, setState] = useState({
@@ -17,15 +19,30 @@ const App = () => {
     setState(result);
   };
 
+  const Home = () => (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+
   const { total, next } = state;
   const result = next || total || '0';
 
+  const Calculator = () => (
+    <div className={styles.container}>
+      <Display result={result} />
+      <ButtonPanel clickHandler={handleClick} />
+    </div>
+  );
   return (
     <>
-      <div className={styles.container}>
-        <Display result={result} />
-        <ButtonPanel clickHandler={handleClick} />
-      </div>
+      <BrowserRouter>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/calculator" component={Calculator} />
+        </Switch>
+      </BrowserRouter>
     </>
   );
 };
